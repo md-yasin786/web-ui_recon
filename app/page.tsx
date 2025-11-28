a4c7d60a1cd1d430b67bd6b5290b5b7a556e80f2
Md-Yasin <mohamed786783@gmail.com>
2025-11-28 20:57:02 +0530

    https://web-api-recon.onrender.com/scan

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
     try {
       // ⛔ This will work AFTER we build the backend at http://localhost:8000/scan
-      const res = await fetch("http://127.0.0.1:8000/scan", {
+      const res = await fetch("https://web-api-recon.onrender.com/scan", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ target }),
